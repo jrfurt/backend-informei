98fbe9745d40728bd1fc917495c0d0206507806d
Clodomir Jr <106870350+jrfurt@users.noreply.github.com>
2024-04-01 17:19:12 -0300

    feat: criação do arquivo .env

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2/promise');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../', '.env') });
+
+console.log(`hello, ${process.env.HELLO}`)
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'maisumcode',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 module.exports = connection;
